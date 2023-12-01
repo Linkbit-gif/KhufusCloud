aabe6bbfbe9076deb423bf5983ed597fdb1eeca9
Ryan Donnelly <51913413+Linkbit-gif@users.noreply.github.com>
2023-12-01 23:05:45 +0000

    Neurological disposition reprieve
    Break

---
+++ Anti.consort.array/CustomControl1.vb
@@ -0,0 +1,39 @@
+﻿' Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+'
+' Step 1a) Using this custom control in a XAML file that exists in the current project.
+' Add this XmlNamespace attribute to the root element of the markup file where it is 
+' to be used:
+'
+'     xmlns:MyNamespace="clr-namespace:Anti.consort.array"
+'
+'
+' Step 1b) Using this custom control in a XAML file that exists in a different project.
+' Add this XmlNamespace attribute to the root element of the markup file where it is 
+' to be used:
+'
+'     xmlns:MyNamespace="clr-namespace:Anti.consort.array;assembly=Anti.consort.array"
+'
+' You will also need to add a project reference from the project where the XAML file lives
+' to this project and Rebuild to avoid compilation errors:
+'
+'     Right click on the target project in the Solution Explorer and
+'     "Add Reference"->"Projects"->[Browse to and select this project]
+'
+'
+' Step 2)
+' Go ahead and use your control in the XAML file. Note that Intellisense in the
+' XML editor does not currently work on custom controls and its child elements.
+'
+'     <MyNamespace:CustomControl1/>
+'
+
+Public Class CustomControl1
+    Inherits System.Windows.Controls.Control
+
+    Shared Sub New()
+        'This OverrideMetadata call tells the system that this element wants to provide a style that is different than its base class.
+        'This style is defined in themes\generic.xaml
+        DefaultStyleKeyProperty.OverrideMetadata(GetType(CustomControl1), New FrameworkPropertyMetadata(GetType(CustomControl1)))
+    End Sub
+
+End Class
